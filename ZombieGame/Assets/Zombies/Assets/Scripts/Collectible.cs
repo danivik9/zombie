7d06cf50ec29536eb9608e7b42b91ce1effd4662
danivik9 <daniela.vikaine@gmail.com>
2026-02-24 21:58:31 +0200

    Collectible script
    Wrote the script for collectables

--- ZombieGame/Assets/Zombies/Assets/Scripts/Collectible.cs
+++ ZombieGame/Assets/Zombies/Assets/Scripts/Collectible.cs
@@ -2,12 +2,10 @@ using UnityEngine;
 
 public class Collectible : MonoBehaviour
 {
-    private void OnTriggerEnter(Collider other)
+   private void OnTriggerEnter (Collider other)
     {
-        // check if a zombie touched the coin
         if (other.CompareTag("Zombie"))
         {
-            // destroy the coin
             Destroy(gameObject);
         }
     }
